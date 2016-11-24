fbb5778ca66af743e44d8e8757c541a1aa93200d
unknown <C:\Users\Admin\AppData\Roaming\The Bat!>
2016-11-24 15:54:16 +0300

    header

--- app/js/common.js
+++ app/js/common.js
@@ -1,10 +1,29 @@
 $(function() {
 
 	// ВЫравнивание блоков по высоте
-	//$(".service-item h4").equalHeights(); 
+	//$(".service-item h4").equalHeights();
 
 	//Слайдер
-	
+	$(".sl").slick({
+		arrows: true,
+		dots: true,
+		responsive: [
+		{
+			breakpoint: 768,
+			settings: {
+				arrows: false,
+				slidesToShow: 3
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				arrows: false,
+				slidesToShow: 1
+			}
+		}
+		]
+	});
 
 	//плавный скрол в хроме
 	try {
